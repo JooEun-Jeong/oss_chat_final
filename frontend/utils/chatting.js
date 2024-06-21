2f1bed684fc3b133b34ad56c8d4379b7de0dcd20
JooEun-Jeong <prichard147@naver.com>
2024-06-21 22:49:31 +0900

    [feat/FE] Can upload image in chat

--- frontend/utils/chatting.js
+++ frontend/utils/chatting.js
@@ -31,9 +31,34 @@ $(document).ready(function () {
           return;
         }
 
+        // #############################################
+        // ###### PHASE 2: Upload Image ################
+        // ### 채팅으로 이미지 업로드 가능 ##########
+        // #############################################
+        var chatBox = $("#chat_box").find(".chatting");
         if (msgSender != sender) {
-          $("#chat_box").find(".chatting").append("<div><div class='receiver'>" + sender + "</div>" + "<div class='opsChat'>" +
-            "<div class='op chat'>" + text.replace(/(?:\r\n|\r|\n)/g, '<br/>') + "</div><div class='timeLeft'>" + time + "</div></div></div>");
+          if (text.includes("![image](")) {
+            var imageUrl = text.match(/\((.*?)\)/)[1];
+            chatBox.append("<div><div class='receiver'>" + msgSender + "</div>" + "<div class='opsChat'>" +
+              "<div class='op chat'><img src='" + imageUrl + "' alt='Uploaded Image' style='max-width: 100%; height: auto;'/></div>" +
+              "<div class='timeLeft'>" + time + "</div></div></div>");
+          } else {
+            chatBox.append("<div><div class='receiver'>" + msgSender + "</div>" + "<div class='opsChat'>" +
+              "<div class='op chat'>" + text.replace(/(?:\r\n|\r|\n)/g, '<br/>') + "</div>" +
+              "<div class='timeLeft'>" + time + "</div></div></div>");
+          }
+        } else {
+          if (text.includes("![image](")) {
+            var imageUrl = text.match(/\((.*?)\)/)[1];
+            chatBox.append("<div class='myChat'>" +
+              "<div class='me chat'><img src='" + imageUrl + "' alt='Uploaded Image' style='max-width: 100%; height: auto;'/></div>" +
+              "<div class='timeRight'>" + time + "</div></div>");
+          } else {
+            chatBox.append("<div class='myChat'>" +
+              "<div class='me chat'>" + text.replace(/(?:\r\n|\r|\n)/g, '<br/>') + "</div>" +
+              "<div class='timeRight'>" + time + "</div></div>");
+          }
+          ////// END PHASE 2 /////////////
         }
         $(".chatting").scrollTop($(".chatting")[0].scrollHeight);
       });
@@ -64,33 +89,101 @@ function get_all_chats(chats) {
       var time = convertToLocalTime(item.sent_at); // ###### PHASE 2: Feat Reserve Message ########
       console.log("time in get_all_chats: " + item.sent_at);
 
-
-      if (sender == current_user) {
-        $("#chat_box").find(".chatting").append("<div class='myChat'>" +
-          "<div class='me chat'>" + item.content.replace(/(?:\r\n|\r|\n)/g, '<br/>') + "</div><div class='timeRight'>" + time + "</div></div>");
-      }
-      else {
-        $("#chat_box").find(".chatting").append("<div><div class='receiver'>" + sender + "</div>" + "<div class='opsChat'>" +
-          "<div class='op chat'>" + item.content.replace(/(?:\r\n|\r|\n)/g, '<br/>') + "</div><div class='timeLeft'>" + time + "</div></div></div>");
+      // #############################################
+      // ###### PHASE 2: Upload Image ################
+      // ### 채팅으로 이미지 업로드 가능 ##########
+      // #############################################
+      var chatBox = $("#chat_box").find(".chatting");
+      if (text.includes("![image](")) {
+        var imageUrl = text.match(/\((.*?)\)/)[1];
+        if (sender == current_user) {
+          chatBox.append("<div class='myChat'>" +
+            "<div class='me chat'><img src='" + imageUrl + "' alt='Uploaded Image' style='max-width: 100%; height: auto;'/></div>" +
+            "<div class='timeRight'>" + time + "</div></div>");
+        } else {
+          chatBox.append("<div><div class='receiver'>" + sender + "</div>" + "<div class='opsChat'>" +
+            "<div class='op chat'><img src='" + imageUrl + "' alt='Uploaded Image' style='max-width: 100%; height: auto;'/></div>" +
+            "<div class='timeLeft'>" + time + "</div></div></div>");
+        }
+        // #############################################
+        // ###### END PHASE 2: Upload Image ################
+        // #############################################
+      } else {
+        if (sender == current_user) {
+          chatBox.append("<div class='myChat'>" +
+            "<div class='me chat'>" + text.replace(/(?:\r\n|\r|\n)/g, '<br/>') + "</div><div class='timeRight'>" + time + "</div></div>");
+        } else {
+          chatBox.append("<div><div class='receiver'>" + sender + "</div>" + "<div class='opsChat'>" +
+            "<div class='op chat'>" + text.replace(/(?:\r\n|\r|\n)/g, '<br/>') + "</div><div class='timeLeft'>" + time + "</div></div></div>");
+        }
       }
     });
     $(".chatting").scrollTop($(".chatting")[0].scrollHeight);
   });
 }
+
 function get_chats(data) {
   $.getJSON("/chat", data, get_all_chats);
 }
 
 function sendClick(event) {
   var date = new Date();
-  var time = convertToLocalTime(date);
-
   var text = $("#text_box").val();
+  var fileInput = document.getElementById('image_input');
+  var file = fileInput.files[0];
 
-  if (text == "") {
-    return;
+  // #############################################
+  // ###### PHASE 2: Upload Image ################
+  // ### 채팅으로 이미지 업로드 가능 ##########
+  // #############################################
+  if (file) {
+    uploadImage(file);
+    // #############################################
+    // ###### END PHASE 2: Upload Image ################
+    // #############################################
+  } else {
+    if (text == "") {
+      return;
+    }
+    sendMessage(text, date);
   }
+}
 
+// #############################################
+// ###### PHASE 2: Upload Image ################
+// ### 채팅으로 이미지 업로드 가능 ##########
+// #############################################
+function uploadImage(file) {
+  $.getJSON("/current_user", function (sender) {
+    $.getJSON("/getroom", { user1: sender, user2: friend }, function (data) {
+      var formData = new FormData();
+      formData.append("current_user_id", sender);
+      formData.append("target_user_id", friend);
+      formData.append("file", file);
+
+      $.ajax({
+        url: "/upload_image",
+        type: "post",
+        data: formData,
+        contentType: false,
+        processData: false,
+        success: function (data) {
+          alert("Image uploaded successfully");
+          fileInput.value = "";
+          sendMessage(data.file_url, new Date(), true);
+        },
+        error: function (request, error) {
+          console.log(JSON.stringify(error));
+        }
+      });
+    });
+  });
+}
+// #############################################
+// ###### END PHASE 2: Upload Image ################
+// #############################################
+
+function sendMessage(text, date, isImage = false) {
   $.getJSON("/current_user", function (sender) {
     $.getJSON("/getroom", { user1: sender, user2: friend }, function (data) {
       var msg = {
@@ -114,29 +207,14 @@ function sendClick(event) {
           data: JSON.stringify(msg),
           contentType: "application/json",
           success: function (data) {
-            $.ajax({
-              url: "/updatelastchat",
-              type: "post",
-              data: JSON.stringify({ "header_id": msg.header_id, "last_chat": text }),
-              contentType: "application/json",
-              success: function (data) {
-                console.log(data);
-              },
-              error: function (request, error) {
-                console.log(JSON.stringify(error));
-              },
-            });
-            console.log(data);
-            console.log(JSON.stringify(msg));
+            updateLastChat(msg.header_id, text);
+            ws.send(JSON.stringify(msg));
           },
           error: function (request, error) {
             console.log(JSON.stringify(error));
           },
         });
-
-        ws.send(JSON.stringify(msg));
-      }
-      else {
+      } else {
         $.ajax({
           url: "/makeroom",
           data: JSON.stringify({ "from_id": sender, "to_id": friend, "to_group": null, "last_chat": text }),
@@ -152,40 +230,57 @@ function sendClick(event) {
               data: JSON.stringify(msg),
               contentType: "application/json",
               success: function (data) {
-                $.ajax({
-                  url: "/updatelastchat",
-                  type: "post",
-                  data: JSON.stringify({ "header_id": msg.header_id, "last_chat": text }),
-                  contentType: "application/json",
-                  success: function (data) {
-                    console.log(data);
-                  },
-                  error: function (request, error) {
-                    console.log(JSON.stringify(error));
-                  },
-                });
-                console.log(data);
-                console.log(JSON.stringify(msg));
+                updateLastChat(msg.header_id, text);
+                ws.send(JSON.stringify(msg));
               },
               error: function (request, error) {
                 console.log(JSON.stringify(error));
               },
             });
-
-            ws.send(JSON.stringify(msg));
           }
-        })
+        });
       }
 
-      $("#chat_box").find(".chatting").append("<div class='myChat'>" +
-        "<div class='me chat'>" + text.replace(/(?:\r\n|\r|\n)/g, '<br/>') + "</div><div class='timeRight'>" + time + "</div></div>");
+      // #############################################
+      // ###### PHASE 2: Upload Image ################
+      // #############################################
+      var chatBox = $("#chat_box").find(".chatting");
+      if (isImage) {
+        chatBox.append("<div class='myChat'>" +
+          "<div class='me chat'><img src='" + text + "' alt='Uploaded Image' style='max-width: 100%; height: auto;'/></div>" +
+          "<div class='timeRight'>" + convertToLocalTime(date) + "</div></div>");
+      } else {
+        chatBox.append("<div class='myChat'>" +
+          "<div class='me chat'>" + text.replace(/(?:\r\n|\r|\n)/g, '<br/>') + "</div>" +
+          "<div class='timeRight'>" + convertToLocalTime(date) + "</div></div>");
+      }
+      // #############################################
+      // ###### END PHASE 2: Upload Image ################
+      // #############################################
 
       $("#text_box").val("");
       $(".chatting").scrollTop($(".chatting")[0].scrollHeight);
       $("#text_box").focus();
     });
-  })
+  });
 }
+
+
+function updateLastChat(header_id, text) {
+  $.ajax({
+    url: "/updatelastchat",
+    type: "post",
+    data: JSON.stringify({ "header_id": header_id, "last_chat": text }),
+    contentType: "application/json",
+    success: function (data) {
+      console.log(data);
+    },
+    error: function (request, error) {
+      console.log(JSON.stringify(error));
+    },
+  });
+}
+
 
 // #############################################
 // ###### PHASE 2: Feat Reserve Message ########
